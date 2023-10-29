4a138d849baa31cf54bdadb63ae1f770dc4b319f
SofieAmalie44 <sofieamaliethorlund@gmail.com>
2023-10-29 14:25:11 +0100

    hamburger and sectioned js

--- musik.js
+++ musik.js
@@ -1,49 +1,64 @@
 let musicMap = new Map();
-
-musicMap.set(65, {mp3: 'https://p.scdn.co/mp3-preview/dde4ed3c04aae7aaf22a5fa2d72bef4d900ba02c', img: "kisspng-color-ink-paint-colorful-ink-5aa6b9448d2693.1552322115208758445782.png", timeToPlay:70, seekTime:22, imgHeight:600, imgWidth:1500})
-musicMap.set(66, {mp3: 'thank god.mp3', img: "pink-dots-unscreen.gif", timeToPlay:180, seekTime:120, imgHeight:800, imgWidth:1500})
-musicMap.set(67, {mp3: 'thank god.mp3', img: "blueball.gif", timeToPlay:180, seekTime:30, imgHeight:400, imgWidth:400})
-musicMap.set(68, {mp3: 'thank god.mp3', img: "disappearing-dot-unscreen.gif", timeToPlay:180, seekTime:40, imgHeight:600, imgWidth:1700})
-musicMap.set(69, {mp3: 'https://p.scdn.co/mp3-preview/dde4ed3c04aae7aaf22a5fa2d72bef4d900ba02c', img: "dots-unscreen.gif", timeToPlay:180, seekTime:100, imgHeight:400, imgWidth:700})
-musicMap.set(70, {mp3: 'deep-house-pluck-25180 (1).mp3', img: "fading-dots-unscreen.gif", timeToPlay:150, seekTime:20, imgHeight:700, imgWidth:700})
-musicMap.set(71, {mp3: 'thank God Hat.wav', img: "loading-dots-unscreen.gif", timeToPlay:180, seekTime:10, imgHeight:800, imgWidth:1500})
-musicMap.set(72, {mp3: 'balafonk-rhythm-148936.mp3', img: "pink-dots-unscreen.gif", timeToPlay:180, seekTime:300, imgHeight:700, imgWidth:700})
-musicMap.set(73, {mp3: 'bio-life-155999.mp3', img: "sideToSideDot-unscreen.gif", timeToPlay:180, seekTime:10, imgHeight:800, imgWidth:1500})
-musicMap.set(74, {mp3: 'cortinilla-de-transicion-1-9753.mp3', img: "green-dots-unscreen.gif", timeToPlay:1000, seekTime:600, imgHeight:1000, imgWidth:1500})
-musicMap.set(75, {mp3: 'bio-life-155999.mp3', img: "flow-wave.gif", timeToPlay:500, seekTime:22, imgHeight:700, imgWidth:1500})
-musicMap.set(76, {mp3: 'deep-house-pluck-25180 (1).mp3', img: "red-bubbles-unscreen.gif", timeToPlay:180, seekTime:20, imgHeight:700, imgWidth:1700})
-musicMap.set(77, {mp3: 'deep-house-pluck-25180 (1).mp3', img: "three_bouncing_dots.gif", timeToPlay:1500, seekTime:100, imgHeight:700, imgWidth:1500})
-musicMap.set(78, {mp3: 'Thank God Snare.wav', img: "moving-size-dots-unscreen.gif", timeToPlay:150, seekTime:102, imgHeight:700, imgWidth:2000})
-musicMap.set(79, {mp3: 'Thank God Kick.wav', img: "bouncing-unscreen.gif", timeToPlay:150, seekTime:200, imgHeight:700, imgWidth:1000})
-
-
-console.log("map:" + musicMap.get(65).mp3);
+//Music data, mapped by keystroke numbers
+musicMap.set(65/*A*/, createMusicData('https://p.scdn.co/mp3-preview/dde4ed3c04aae7aaf22a5fa2d72bef4d900ba02c', "animated-mesh-line--unscreen.gif", 70, 22, 600, 1500));
+musicMap.set(66/*B*/, createMusicData('thank god.mp3', "pink-dots-unscreen.gif", 180, 120, 800, 1500));
+musicMap.set(67/*C*/, createMusicData('thank god.mp3', "blueball.gif", 180, 30, 400, 400));
+musicMap.set(68/*D*/, createMusicData('thank god.mp3', "disappearing-dot-unscreen.gif", 180, 40, 600, 1700));
+musicMap.set(69/*E*/, createMusicData('https://p.scdn.co/mp3-preview/dde4ed3c04aae7aaf22a5fa2d72bef4d900ba02c', "dots-unscreen.gif", 180, 100, 400, 700));
+musicMap.set(70/*F*/, createMusicData('deep-house-pluck-25180 (1).mp3', "fading-dots-unscreen.gif", 150, 20, 700, 700));
+musicMap.set(71/*G*/, createMusicData('Thank God Hat.wav', "loading-dots-unscreen.gif", 180, 10, 800, 1500));
+musicMap.set(72/*H*/, createMusicData('balafonk-rhythm-148936.mp3', "pink-dots-unscreen.gif", 180, 300, 700, 700));
+musicMap.set(73/*I*/, createMusicData('bio-life-155999.mp3', "sideToSideDot-unscreen.gif", 180, 10, 800, 1500));
+musicMap.set(74/*J*/, createMusicData('cortinilla-de-transicion-1-9753.mp3', "green-dots-unscreen.gif", 1000, 600, 1000, 1500));
+musicMap.set(75/*K*/, createMusicData('bio-life-155999.mp3', "flow-wave.gif", 500, 22, 700, 1500));
+musicMap.set(76/*L*/, createMusicData('deep-house-pluck-25180 (1).mp3', "red-bubbles-unscreen.gif", 180, 20, 700, 1700));
+musicMap.set(77/*M*/, createMusicData('deep-house-pluck-25180 (1).mp3', "three_bouncing_dots.gif", 1500, 100, 700, 1500));
+musicMap.set(78/*N*/, createMusicData('Thank God Snare.wav', "moving-size-dots-unscreen.gif", 180, 10, 700, 2000));
+musicMap.set(79/*O*/, createMusicData('Thank God Kick.wav', "bouncing-unscreen.gif", 150, 20, 700, 1000));
+musicMap.set(80/*G*/, createMusicData('Thank God Hat.wav', "flowting_buubles.gif", 180, 100, 800, 1500));
+musicMap.set(81/*H*/, createMusicData('balafonk-rhythm-148936.mp3', "moving-size-dots-unscreen.gif", 180, 30, 700, 700));
+musicMap.set(82/*I*/, createMusicData('bio-life-155999.mp3', "liquid-dots-opt-unscreen.gif", 180, 200, 800, 1500));
+musicMap.set(83/*J*/, createMusicData('cortinilla-de-transicion-1-9753.mp3', "bluecircle.png", 1000, 60, 1000, 1500));
+musicMap.set(84/*K*/, createMusicData('bio-life-155999.mp3', "blod.gif", 500, 202, 700, 1500));
+musicMap.set(85/*L*/, createMusicData('deep-house-pluck-25180 (1).mp3', "fading-dots-unscreen.gif", 180, 120, 700, 1700));
+musicMap.set(86/*M*/, createMusicData('deep-house-pluck-25180 (1).mp3', "bouncing-red-ball.gif", 1500, 15, 700, 1500));
+musicMap.set(87/*N*/, createMusicData('Thank God Snare.wav', "motion-unscreen.gif", 180, 200, 700, 2000));
+musicMap.set(88/*O*/, createMusicData('Thank God Kick.wav', "three_bouncing_dots.gif", 150, 20, 700, 1000));
+musicMap.set(89/*N*/, createMusicData('Thank God Snare.wav', "pulsating-animation-unscreen (1).gif", 180, 100, 700, 2000));
+musicMap.set(90/*O*/, createMusicData('Thank God Kick.wav', "flowting_buubles.gif", 150, 10, 700, 1000));
+const tune = new Audio();
+const getImgDiv = document.querySelector("#imgDiv");
+const getImg = document.querySelector("#blue");
 
 document.addEventListener('keydown', function(e) {
     let entry = musicMap.get(e.keyCode);
     if (entry) {
-        play(entry.mp3, entry.timeToPlay, entry.seekTime, entry.img, entry.imgHeight, entry.imgWidth);
+        playTune(entry.mp3, entry.seekTime);
+        showImg(entry.img, entry.imgHeight, entry.imgWidth);
+        setTimeout(() => {
+            stopTune();
+        }, entry.timeToPlay);
     }
     return entry;
 });
 
-function play( audio_path, time_in_miliSec, seek_time, image, image_height, image_width) {
-    //if (beep) beep.pause();
-    let beep = new Audio( audio_path);
-    beep.controls = true;
-    beep.loop = true;
-    beep.currentTime = seek_time;
-    beep.play();
-    const getImgDiv = document.querySelector("#imgDiv");
-    const getImg = document.querySelector("#blue");
+function stopTune() {
+    tune.pause();
+}
+
+function playTune(audio_path, seek_time) {
+    tune.src = audio_path;
+    tune.loop = true;
+    tune.currentTime = seek_time;
+    tune.play();
+}
+
+function showImg(image, image_height, image_width) {
     getImg.src = image + "?xx=" + new Date().getMilliseconds();
     getImg.height = image_height
     getImg.width = image_width
     getImgDiv.hidden = false;
-    setTimeout(() => {
-        beep.pause();
-        getImgDiv.hidden = true;
-        }, time_in_miliSec);
+    getImg.hidden = false;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -59,3 +74,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     })
 });
+
+function createMusicData(mp3, img, timeToPlay, seekTime, imgHeight, imgWidth) {
+    return {mp3:mp3, img:img, timeToPlay:timeToPlay, seekTime:seekTime, imgHeight:imgHeight, imgWidth:imgWidth};
+}
+
+
